20350de8b6f5342858b4ca95092b0c1555711d07
Mauricio Castro <mauricio@niclabs.cl>
2013-10-22 04:07:19 -0400

    Added basic AI

---
+++ Assets/Scripts/BasicAI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicAI : MonoBehaviour {
+	
+	private GameObject player;
+	public float enemySpeed = 4.0f;
+	// Use this for initialization
+	void Start () {
+		
+		player = GameObject.FindGameObjectWithTag("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		Vector3 start = transform.position;
+		Vector3 end = Vector3.MoveTowards(start, player.rigidbody.position, enemySpeed * Time.deltaTime);
+		transform.position = end;
+	
+	}
+}
