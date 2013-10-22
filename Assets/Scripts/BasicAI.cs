67078e17e383c9c551f10059600714940c9a35d8
Mauricio Castro <mauricio@niclabs.cl>
2013-10-22 05:09:22 -0400

    Commit Before pull

--- Assets/Scripts/BasicAI.cs
+++ Assets/Scripts/BasicAI.cs
@@ -5,9 +5,11 @@ public class BasicAI : MonoBehaviour {
 	
 	private GameObject player;
 	public float enemySpeed = 4.0f;
+	private bool isNear;
 	// Use this for initialization
 	void Start () {
 		
+		isNear = false;
 		player = GameObject.FindGameObjectWithTag("Player");
 	
 	}
@@ -15,9 +17,27 @@ public class BasicAI : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 		
-		Vector3 start = transform.position;
-		Vector3 end = Vector3.MoveTowards(start, player.rigidbody.position, enemySpeed * Time.deltaTime);
-		transform.position = end;
+		if (!isNear) {
+			Vector3 start = transform.position;
+			Vector3 end = Vector3.MoveTowards(start, player.rigidbody.position, enemySpeed * Time.deltaTime);
+			transform.position = end;
+			
+		}else{
+			
+		}
 	
+	}
+	
+	void OnTriggerEnter(Collider target){
+		
+		if(target.transform.tag.Equals("Player")){
+			isNear = true;	
+		}
+	}
+	
+	void OnTriggerExit(Collider target){
+		if(target.transform.tag.Equals("Player")){
+			isNear = false;	
+		}
 	}
 }
