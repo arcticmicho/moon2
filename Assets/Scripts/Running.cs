3ac509b6d74dff2f38bcc44ced3028329429afd5
Mauricio Castro <mauricio@niclabs.cl>
2013-10-22 06:05:54 -0400

    Added Stamina

--- Assets/Scripts/Running.cs
+++ Assets/Scripts/Running.cs
@@ -6,19 +6,32 @@ public class Running : MonoBehaviour {
 	public WalkController walker;
 	public float speedWalk = 6.0F;
 	public float speedRun = 20.0F;
+	public float runStamina = 30.0f;
+	private float runCount;
+	public bool canRun;
 
 	// Use this for initialization
 	void Start () {
 		walker.setMoveSpeed(speedWalk);
+		runCount = runStamina;
+		canRun = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		
-		if (Input.GetKey(KeyCode.Q)){
+		Debug.Log(runCount);
+		
+		if (Input.GetKey(KeyCode.Q) && runCount>0 && canRun){
+			runCount -= Time.deltaTime;
 			if (walker.speed != speedRun){
 				walker.setMoveSpeed(speedRun);
 				Debug.Log("Aumente Velocidad");
+			}
+			if(runCount < 0){
+				runCount =0;
+				canRun = false;
+				Debug.Log("no puede correr");
 			}
 			
 		}else{
@@ -26,6 +39,11 @@ public class Running : MonoBehaviour {
 				walker.setMoveSpeed(speedWalk);
 				Debug.Log("Baje Velocidad");
 			}
+			if(runCount>runStamina){
+				runCount = runStamina;
+				canRun = true;
+				Debug.Log("Puede Correr");
+			}else{runCount += Time.deltaTime*3;}
 		}
 	
 	}
